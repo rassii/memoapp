c9d20847f0972850a82457eddd8c5a6babf45df1
rassii <tatsuo.igarashi0601@gmail.com>
2020-03-17 15:43:51 +0900

    next commit

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styles from './main.css';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>mamamamma~n</Text>
-    </View>
-  );
+import BodyText from './src/elements/BodyText';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View className={styles.container}>
+
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbarTitle}>MEMOT</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoList}>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2017/10/10</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2017/10/10</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2017/10/10</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2017/10/10</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2017/10/10</Text>
+          </View>
+
+        </View>
+
+        <View>
+          <Text>+</Text>
+        </View>
+
+      </View>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
