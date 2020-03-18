1e2a7ef967ac7eb95e0ae91d51bc2f69fe5cbf39
rassii <tatsuo.igarashi0601@gmail.com>
2020-03-18 13:10:18 +0900

    third commit

--- App.js
+++ App.js
@@ -1,51 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import styles from './main.css';
+import from './main.css';
 
 import BodyText from './src/elements/BodyText';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View className={styles.container}>
+      <View className="container">
 
-        <View style={styles.appbar}>
+        <View className="appbar">
           <View>
-            <Text style={styles.appbarTitle}>MEMOT</Text>
+            <Text className="appbarTitle">MEMOT</Text>
           </View>
-        </View>
-
-        <View style={styles.memoList}>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-        </View>
-
-        <View>
-          <Text>+</Text>
         </View>
 
       </View>
