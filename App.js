fc2aea25fd1a0952d76c4dbaa9c770d4ab9e6f16
rassii <tatsuo.igarashi0601@gmail.com>
2020-03-19 16:06:28 +0900

    fifth commit

--- App.js
+++ App.js
@@ -1,51 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import BodyText from './src/elements/BodyText';
+import Appbar from './src/components/Appbar';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
 
-        <View style={styles.appbar}>
-          <View>
-            <Text style={styles.appbarTitle}>MEMOT</Text>
-          </View>
-        </View>
+        <Appbar />
 
-        <View style={styles.memoList}>
+        <MemoListScreen />
 
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-          <View style={styles.memoListItem}>
-            <Text style={styles.memoTitle}>講座のアイテム</Text>
-            <Text style={styles.memoDate}>2017/10/10</Text>
-          </View>
-
-        </View>
-
-        <View style={styles.memoAddButton}>
-          <Text style={styles.memoAddButtonTitle}>+</Text>
-        </View>
       </View>
     );
   }
@@ -58,63 +25,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 72,
-  },
-  memoAddButton: {
-    position: 'absolute',
-    bottom: 32,
-    right: 32,
-    width: 48,
-    height: 48,
-    backgroundColor: '#E31676',
-    borderRadius: 24,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-  },
-  memoAddButtonTitle: {
-    color: '#fff',
-    fontSize: 32,
-    lineHeight: 32,
-  },
-  memoList: {
-    width: '100%',
-    flex: 1,
-  },
-  memoListItem: {
-    backgroundColor: '#fff',
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
-  },
-  memoTitle: {
-    fontSize: 18,
-    marginBottom: 4,
-  },
-  memoDate: {
-    fontSize: 12,
-    color: '#a2a2a2',
-  },
-  appbar: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    height: 70,
-    paddingTop: 30,
-    backgroundColor: '#265365',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 0 },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    zIndex: 10,
-  },
-  appbarTitle: {
-    color: '#fff',
-    fontSize: 18,
   },
 });
---
+++ src/elements/CircleButton.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class CircleButton extends React.Component {
+  render() {
+    return (
+      <View style={styles.CircleButton}>
+        <Text style={styles.CircleButtonTitle}>
+          {this.props.children}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  CircleButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  CircleButtonTitle: {
+    color: '#fff',
+    fontSize: 32,
+    lineHeight: 32,
+  },
+});
+
+export default CircleButton;
